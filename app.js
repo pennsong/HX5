df69f254c141227d001d2884e181f8989fa5cd7d
Penn Song <pennsong07@gmail.com>
2015-02-16 17:18:56 +0800

    temp save

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ var bodyParser = require('body-parser');
 var app = express();
 app.http().io();
 
-var db = require('./lib/db');
-app = require('./routes/api')(app, db);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,6 +19,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var db = require('./lib/db');
+app = require('./routes/api')(app, db);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,14 @@
 var jwt = require('jwt-simple');
 var moment = require('moment');
 var async = require("async");
+var http = require('http');
 
 function apiRouter(app, db)
 {
     app.set('jwtTokenSecret', 'ppToken');
 
-    app.get('/login', function(req, res) {
-        if (!(req.query.username && req.query.password))
-        {
-            res.statusCode = 400;
-            res.json({result: '用户名和密码都必须填写!'});
-            return;
-        }
+    function login(req, res){
+        var loginResult = {};
 
         function finalCallback(err, result) {
             if (err){
@@ -20,16 +16,17 @@ function apiRouter(app, db)
                 res.json({result: err.toString()});
             }
             else{
-                res.json({result: result});
+                res.json(result);
             }
         }
 
         async.waterfall([
                 function(next){
+                    //验证用户名密码
                     db.User.findOne(
                         {
-                            username: req.query.username,
-                            password: req.query.password
+                            username: req.body.username,
+                            password: req.body.password
                         },
                         next
                     );
@@ -43,9 +40,10 @@ function apiRouter(app, db)
                         res.statusCode = 401;
                         res.json({result: '用户名或密码错误!'});
                     }
+                    //用户名密码正确
                     else
                     {
-                        //用户名密码正确
+                        //更新cid和token
                         var expires = moment().add(100, 'year').valueOf();
                         var token = jwt.encode({
                             iss: result.id,
@@ -56,21 +54,145 @@ function apiRouter(app, db)
                             {
                                 $set:
                                 {
-                                    token: token
+                                    token: token,
+                                    cid: req.body.cid
                                 }
                             },
                             function (err, numberAffected, raw)
                             {
-                                next(err, token);
+                                loginResult.token = token;
+                                next(err, null);
                             }
                         );
                     }
+                },
+                function(result, next)
+                {
+                    //get meets
+                    getMeets(req.body.username, next);
+                },
+                function(result, next)
+                {
+                    loginResult.meets = result;
+                    //get friends
+                    getFriends(req.body.username, next);
+
+                },
+                function(result, next)
+                {
+                    loginResult.friends = result;
+                    next(null, loginResult);
+                }
+            ],
+            finalCallback
+        );
+    }
+
+    function getMeets(username, next){
+        db.Meet.find(
+            {
+                $or: [
+                    {"creater.username": username},
+                    {"target.username": username}
+                ],
+                status: {$ne:"成功"}
+            })
+            .sort('-_id')
+            .exec(next);
+    }
+
+
+    function getFriends(username, next){
+        db.Friend.find(
+            {
+                $or: [
+                    {"creater.username": username},
+                    {"target.username": username}
+                ]
+            })
+            .sort('-_id')
+            .exec(next);
+    }
+
+    function searchLoc(keyword, callback){
+        var ak = "F9266a6c6607e33fb7c3d8da0637ce0b";
+        var output = "json";
+        var radius = "2000";
+        var scope = "1";
+        var data = "query=" + encodeURIComponent(keyword);
+        data += "&ak=" + ak;
+        data += "&output=" + output;
+        data += "&radius=" + radius;
+        data += "&scope=" + scope;
+        data += "&location=" + "31.209335300000003" + "," + "121.59487019999999";
+        data += "&filter=sort_name:distance";
+
+        var options = {
+            host: 'api.map.baidu.com',
+            port: 80,
+            path: '/place/v2/search?' + data
+        };
+
+        http.get(options, function(res, data) {
+            res.setEncoding('utf8');
+            result = "";
+            res.on("data", function(chunk) {
+                result += chunk;
+            });
+            res.on('end', function () {
+                callback(null, JSON.parse(result));
+            });
+
+        }).on('error', function(e) {
+            callback(e);
+        });
+    }
+
+
+    app.post('/login', function(req, res) {
+        if (!(req.body.username && req.body.password))
+        {
+            res.statusCode = 400;
+            res.json({result: '用户名和密码都必须填写!'});
+            return;
+        }
+
+        login(req, res);
+    });
+
+    app.post('/register', function(req, res) {
+        function finalCallback(err, result) {
+            if (err){
+                res.statusCode = 400;
+                res.json({result: err.toString()});
+            }
+            else{
+                res.json({result: result});
+            }
+        }
+
+        async.waterfall([
+                function(next)
+                {
+                    db.User.create(
+                        {
+                            username: req.body.username,
+                            password: req.body.password,
+                            nickname: req.body.nickname,
+                            token: 'fake',
+                            'specialInfo.sex': req.body.sex
+                        },
+                        next
+                    );
+                },
+                function(result, next)
+                {
+                    login(req, res);
                 }
             ],
             finalCallback
         );
     });
-
 
     app.all("*", function(req, res, next){
         var token = (req.body && req.body.access_token)
@@ -110,9 +232,46 @@ function apiRouter(app, db)
         }
     });
 
+    app.post('/sendMeetCheck', function(req, res) {
+        if (req.user.specialInfoTime
+            && req.user.specialInfoTime > moment(moment().format('YYYY-MM-DD')).valueOf()
+            && req.user.lastLocationTime > moment().add(-5, 'm').valueOf()
+            && req.user.lastMeetCreateTime > moment().add(-30, 's').valueOf()
+            )
+        {
+            res.json({result: 'yes'});
+        }
+        else
+        {
+            res.json({result: 'no'});
+        }
+    });
+
+    app.post('/getNearLocation', function(req, res) {
+        searchLoc('1', function(err, result){
+            if (err)
+            {
+                res.statusCode = 400;
+                res.json({result: '非法请求!'});
+            }
+            else
+            {
+                res.json(result);
+            }
+        });
+    });
+
+
+
     app.get('/', function(req, res) {
         res.render('index', { title: 'api' + req.user });
     });
+
+
+
+
+
+
 
     return app;
 }
